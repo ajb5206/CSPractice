20a532a530e1753ff9c610dea77efd192a4ffb4c
Alex Bertotto <ajbertotto@gmail.com>
2025-08-20 11:22:26 -0700

    loop through both strings and compare characters, breaking the loop if a charachter is not in the opposite string

--- ps1a.py
+++ ps1a.py
@@ -309,7 +309,16 @@ def same_chars(s1, s2):
     versa. If a character only exists in one of s1 or s2, returns False.
     """
     # Your code here
+    count = 0
     for char in s1:
-        if char in s2:
-            return True
-    return False
+        if char not in s2:
+            return False
+    for char2 in s2:
+        if char2 not in s1:
+            return False
+    return True
+
+print(same_chars("abc", "cab"))     # prints True
+print(same_chars("abccc", "caaab")) # prints True
+print(same_chars("abcd", "cabaa"))  # prints False
+print(same_chars("abcabc", "cabz")) # prints False
